73e3da053994759731e7ec0c94ac0484ba56796b
Daniel Schechtman <dschechtman@carthage.edu>
2025-07-02 23:29:13 -0500

    Fixed bug with caching and added caching color for grouping

--- src/Timed Functions/Daily/GroupWeeklyCharges.ts
+++ src/Timed Functions/Daily/GroupWeeklyCharges.ts
@@ -1,31 +1,5 @@
 
 
 function GroupWeeklyCharges() {
-    const QUERY = __Cache_Utils_QueryAllAttrs()
-
-    if (!QUERY) { return }
-
-    for (let query of QUERY) {
-        const SHEET = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(WEEKLY_CREDIT_CHARGES_TAB_NAME)
-        const COLOR_RANGE = SHEET?.getRange(query.range_str)
-        const GROUP_RANGE = SHEET?.getRange(query.grouping_range)
-        const COLOR = query.range_shade
-        let group
-
-        try {
-            group = SHEET!.getRowGroup(GROUP_RANGE!.getRowIndex(), 1)
-            group?.remove()
-            GROUP_RANGE?.shiftRowGroupDepth(1)
-            group = SHEET!.getRowGroup(GROUP_RANGE!.getRowIndex(), 1)
-        } catch {
-            GROUP_RANGE?.shiftRowGroupDepth(1)
-            group = SHEET!.getRowGroup(GROUP_RANGE!.getRowIndex(), 1)
-        }
-
-        if (new Date() > new Date(query.date) && !group?.isCollapsed()) {
-            GROUP_RANGE?.collapseGroups()
-            COLOR_RANGE?.setBackground(COLOR)
-        }
-    }
-
+    __Util_GroupCurrentMonthCharges()
 }
